1b2da1398295b206ad02b2198609acec83df3bff
brendan <bgoldber@oswego.edu>
2017-03-28 17:55:39 -0400

    Fixing error with files

--- server/server.js
+++ server/server.js
@@ -1,19 +1,36 @@
-var express = require('express');
-var app = express();
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;        // set our port
 
-var router = express.Router();
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-router.get('/',function(req,res){
-  res.json({message: 'Welcome to my API'});
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
-app.use('/api',router);
+// more routes for our API will happen here
 
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
 app.listen(port);
-
-console.log('Using port: ' + port);
+console.log('Magic happens on port ' + port);
